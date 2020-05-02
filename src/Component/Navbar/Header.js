4e01e991bf8d990666ec57071817c59c924e645d
jai <jai.arora3@s.amity.edu>
2020-05-02 16:30:37 +0530

    student signin login

--- src/Component/Navbar/Header.js
+++ src/Component/Navbar/Header.js
@@ -2,17 +2,42 @@ import React, { Component } from "react";
 import { Row, Col } from "antd";
 import { Nav, Navbar } from "react-bootstrap";
 
+import { Modal, Button } from 'antd';
+import { Tabs } from 'antd';
+import RegistrationForm from "../regisForm";
+import LoginForm from "../LoginForm";
+
 import "./Header.css";
 
 class header extends Component {
-  state = { visible: false };
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: false
+    }
+  }
 
   showModal = () => {
     this.setState({
       visible: true,
     });
   };
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
   render() {
+    const { TabPane } = Tabs;
     return (
       <div>
         <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
@@ -22,7 +47,7 @@ class header extends Component {
             id="responsive-navbar-nav"
             className="justify-content-end"
           >
-        
+
             <Nav>
               <Nav.Link href="#deets">About Us</Nav.Link>
               <Nav.Link href="#deets">Courses</Nav.Link>
@@ -34,6 +59,25 @@ class header extends Component {
             </Nav>
           </Navbar.Collapse>
         </Navbar>
+        <Modal
+          title="Sign In | Sign Up"
+          mask={false}
+          width={800}
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <Tabs defaultActiveKey="1" onChange={this.callback}>
+            <TabPane tab="Sign Up" key="1">
+              <RegistrationForm />
+
+            </TabPane>
+            <TabPane tab="Sign In" key="2">
+              <LoginForm />
+            </TabPane>
+          </Tabs>
+        </Modal>
+
 
         <br />
         <br />
@@ -41,8 +85,8 @@ class header extends Component {
         <Row className="heading">
           <h5>WHAT ARE YOU LOOKING FOR .......</h5>
         </Row>
-     
-    
+
+
       </div>
     );
   }
