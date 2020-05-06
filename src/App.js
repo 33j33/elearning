7e0e698e3c1d4a23e29c2099c80e19dd5741c9d6
Vikas Prashar <vikasprashar99@gmail.com>
2020-05-06 15:09:12 +0530

    added teacher side nav bar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Test from "./Component/test";
 import Homepage from "./Component/homePage";
-
+import Teacherdashboard from './Component/teacherDashboard/dashboard'
 class App extends Component {
   render() {
     return (
@@ -12,6 +12,8 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={Homepage} />
             <Route path="/test" exact component={Test} />
+            <Route path="/teacherDashboard" exact component={Teacherdashboard} />
+
           </Switch>
         </Router>
       </div>
