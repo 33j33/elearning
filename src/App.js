ec6582b6ed794b08ffaf3b1a91d37c0cfb42195d
yash-khanna <kaydeeit@gmail.com>
2020-05-06 15:16:01 +0530

    Teacher_Dashboard_Payment.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Test from "./Component/test";
 import Homepage from "./Component/homePage";
+import Dashboard from "./Component/teacherDashboard/dashboard";
 
 class App extends Component {
   render() {
@@ -12,6 +13,11 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={Homepage} />
             <Route path="/test" exact component={Test} />
+            <Route
+              path="/teacherDashboard/payment"
+              exact
+              component={Dashboard}
+            />
           </Switch>
         </Router>
       </div>
--- src/Component/teacherDashboard/dashboard.js
+++ src/Component/teacherDashboard/dashboard.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Payment from "./payment";
+
+/* Code Written By: yash Khanna 
+This is the main file "dashboard.js" which links to other files like payment.js
+To access Payment .js:
+Please go to the link on your localhost
+Link : http://localhost:3000/teacherDashboard/payment */
 
 class dashboard extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Payment />
+      </div>
+    );
+  }
 }
 
 export default dashboard;
