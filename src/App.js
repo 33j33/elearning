70e15978a1b00e2442285e3045dc1c9a918b161a
tavishinarang <tavishinarang@gmail.com>
2020-05-07 01:09:37 +0530

    Tavishi Narang

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Test from "./Component/test";
 import Homepage from "./Component/homePage";
+import Profile from "./Component/teacherDashboard/TeacherProfile";
+/*Code by Tavishi, used http://localhost:3000/teacherDashboard/Teacherprofile to access it*/
 
 class App extends Component {
   render() {
@@ -12,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={Homepage} />
             <Route path="/test" exact component={Test} />
+            <Route path="/teacherDashboard/TeacherProfile" exact component={Profile}/>
           </Switch>
         </Router>
       </div>
--- src/Component/teacherDashboard/dashboard.js
+++ src/Component/teacherDashboard/dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import TeacherProfile from "./TeacherProfile";
+/*Code by Tavishi, used http://localhost:3000/teacherDashboard/Teacherprofile to access it*/
 
 class dashboard extends Component {
     render() {
         return (
             <div>
-                
+                <TeacherProfile/>
             </div>
         );
     }
