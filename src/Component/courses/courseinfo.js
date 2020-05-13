702336d2c929335979f5a7787d80c58ffd57719b
Rajat012k <rajatdav2000@gmail.com>
2020-05-13 11:02:41 +0530

    courseinfo

--- src/Component/courses/courseinfo.js
+++ src/Component/courses/courseinfo.js
@@ -12,27 +12,32 @@ class courseinfo extends Component {
       confirmLoading: false,
       timeSlot: [],
       days: "",
+      selectedday: "",
+      time: "",
       allDays: [],
-      value1: "",
-      value2: "",
-      value3: "",
+      showButton: false,
+      showPanel: true,
       showField: false,
       cardData: JSON.parse(sessionStorage.getItem("cardData")),
     };
     this.selectedDay = this.selectedDay.bind(this);
     this.onClicked = this.onClicked.bind(this);
+    this.onChange1 = this.onChange1.bind(this);
+    this.getSchedule = this.getSchedule.bind(this);
+    this.submitschedule = this.submitschedule.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleAdd = () => {
-    this.setState({ allDays: [...this.state.allDays, ""] });
-  };
+  // handleAdd = () => {
+  //   this.setState({ allDays: [...this.state.allDays, ""] });
+  // };
 
-  removeRow = (index) => {
-    this.state.allDays.splice(index, 1);
-    this.setState({ allDays: this.state.allDays });
-    this.final_selectedtime.pop(index);
-    console.log(this.final_selectedtime);
-  };
+  // removeRow = (index) => {
+  //   this.state.allDays.splice(index, 1);
+  //   this.setState({ allDays: this.state.allDays });
+  //   this.final_selectedtime.pop(index);
+  //   console.log(this.final_selectedtime);
+  // };
 
   //SELECTED DAYS AND TIME SLOTS
   selectedDay(e) {
@@ -41,19 +46,18 @@ class courseinfo extends Component {
   }
   handleChange = (value) => {
     console.log(value);
-    this.setState({ days: value });
     console.log(this.state.cardData.course_schedule);
     for (const i in this.state.cardData.course_schedule) {
       console.log(this.state.cardData.course_schedule[i].day);
       if (this.state.cardData.course_schedule[i].day === value) {
-        console.log(i.day);
         this.setState({
           timeSlot: this.state.cardData.course_schedule[i].time,
           showField: true,
         });
       }
     }
-    console.log(this.state.showField);
+    this.setState({ days: value });
+    console.log("day", this.state.days);
   };
 
   final_selectedtime = [];
@@ -68,43 +72,70 @@ class courseinfo extends Component {
 
   children = [];
 
-  componentDidMount() {
-    console.log(this.state.cardData);
-    console.log(this.state.cardData.course_schedule);
+  // componentDidMount() {
+  //   console.log(this.state.cardData);
+  //   console.log(this.state.cardData.course_schedule);
 
-    for (let i = 8; i < 22; i = i + 1) {
-      this.children.push(
-        <Option key={i + "-" + (Number(i) + 1)}>
-          {i + "-" + (Number(i) + 1)}
-        </Option>
-      );
-    }
-  }
-  componentWillMount() {
-    console.log(this.props.data);
-  }
-
+  //   for (let i = 8; i < 22; i = i + 1) {
+  //     this.children.push(
+  //       <Option key={i + "-" + (Number(i) + 1)}>
+  //         {i + "-" + (Number(i) + 1)}
+  //       </Option>
+  //     );
+  //   }
+  // }
   //For radio button group
+  scheduleArray = [];
+  selectedday = "";
+  getSchedule(day) {
+    console.log("days is", day);
+    this.setState({ days: day });
+  }
+
+  getday = (day) => {
+    console.log("day", day);
+    this.selectedday = day;
+    this.setState({ days: day });
+  };
   onChange1 = (e) => {
-    console.log("radio checked1", e.target.value);
-    this.setState({
-      value1: e.target.value,
-    });
-  };
+    console.log("r", this.selectedday);
 
-  onChange2 = (e) => {
-    console.log("radio checked2", e.target.value);
-    this.setState({
-      value2: e.target.value,
-    });
+    const body = {
+      day: this.state.days,
+      time: e.target.value,
+    };
+    this.scheduleArray.push(body);
+    console.log(e.target);
+    console.log("radio checked1", this.scheduleArray);
   };
+  submitschedule() {
+    this.setState({ showButton: true });
+    this.setState({ showPanel: false });
 
-  onChange3 = (e) => {
-    console.log("radio checked3", e.target.value);
-    this.setState({
-      value3: e.target.value,
-    });
-  };
+    // axios
+    // .post("https://elearningserver.herokuapp.com/teacherlogin", values)
+    // .then((response) => {
+    //   console.log(response);
+    //   this.formRef.current.resetFields();
+    //   successForlogin();
+    //   this.setState({
+    //     showButton: true,
+    //   });
+
+    //   window.localStorage.setItem(
+    //     "currentUser",
+    //     JSON.stringify({ token, email, phone, username, teacherid })
+    //   );
+    //   this.setState({ showField: true, username: username });
+    //   this.props.history.push("teacher/dashboard");
+    // })
+    // .catch((error) => {
+    //   if (error.response !== undefined) {
+    //     console.log(error.response);
+    //     errorForlogin();
+    //   }
+    // });
+  }
 
   //For collapse
   callback = (e) => {
@@ -143,27 +174,26 @@ class courseinfo extends Component {
           onChange={this.callback}
         >
           <Panel header="Full Course" key="2">
-            <p>
-              Teacher's avalaible slots:
-              <ul>
-                {this.state.cardData.course_schedule.map((i) => (
-                  <li>
-                    {i.day}
-                    {i.time.map((i) => (
-                      <Radio.Group
-                        onChange={this.onChange1}
-                        value={this.state.value1}
-                        style={{ marginLeft: 30 }}
-                      >
-                        <Radio value={1}>{i}</Radio>
-                      </Radio.Group>
-                    ))}
-                  </li>
-                ))}
-              </ul>
-            </p>
-            <p>
-              Course Fees: Rs.{this.state.cardData.course_price}
+            Teacher's avalaible slots:
+            <ul>
+              {this.state.cardData.course_schedule.map((i) => (
+                <li onFocus={() => this.getSchedule(i.day)}>
+                  {i.day}
+                  <Radio.Group
+                    onChange={this.onChange1}
+                    onClick={() => this.getday(i.day)}
+                    style={{ marginLeft: 30 }}
+                    // value={j}
+                    options={i.time}
+                  ></Radio.Group>
+                </li>
+              ))}
+            </ul>
+            <Row>
+              <Button onClick={this.submitschedule}>submit</Button>
+            </Row>
+            Course Fees: Rs.{this.state.cardData.course_price}
+            {this.state.showButton ? (
               <Button
                 type="primary"
                 htmlType="submit"
@@ -171,50 +201,59 @@ class courseinfo extends Component {
               >
                 Pay
               </Button>
-            </p>
+            ) : null}
           </Panel>
-          <Panel header="Half Course" key="3" style={{ marginTop: 10 }}>
-            <Row>Choose slots here:</Row>
-            {/* {this.state.allDays.map((day, index) => {
-              return ( */}
-            <Row justify="space-between">
-              <Col span={9}>
-                {/* <Select
-                      placeholder="Select Day"
-                      onChange={this.selectedDay}
-                      style={{ width: "100%" }}
-                    > */}
-                <Select style={{ width: 200 }} onChange={this.handleChange}>
-                  {this.state.cardData.course_schedule.map((i) => (
-                    <Option value={i.day}>{i.day}</Option>
-                  ))}
-                </Select>
-              </Col>
-              <Col span={9}>
-                {this.state.showField ? (
-                  <Select style={{ width: 200 }} onChange={this.handleChange1}>
-                    {this.state.timeSlot.map((j) => (
-                      <Option value={j}>{j}</Option>
+          {this.state.showPanel ? (
+            <Panel header="Half Course" key="3" style={{ marginTop: 10 }}>
+              <Row>Choose slots here:</Row>
+
+              <Row justify="space-between">
+                <Col span={9}>
+                  <Select
+                    style={{ width: 200 }}
+                    value={this.state.days}
+                    onChange={this.handleChange}
+                  >
+                    {this.state.cardData.course_schedule.map((i) => (
+                      <Option value={i.day}>{i.day}</Option>
                     ))}
                   </Select>
-                ) : null}
-              </Col>
-            </Row>
-            <br />
-            <Row>
-              <Col>
-                {" "}
-                Course Fees: Rs.{this.state.cardData.course_price}
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  style={{ marginLeft: 15 }}
-                >
-                  Pay
-                </Button>
-              </Col>
-            </Row>
-          </Panel>
+                </Col>
+                <Col span={9}>
+                  {this.state.showField ? (
+                    <Select
+                      style={{ width: 200 }}
+                      onChange={this.handleChange1}
+                    >
+                      {this.state.timeSlot.map((j) => (
+                        <Option value={j}>{j}</Option>
+                      ))}
+                    </Select>
+                  ) : null}
+                </Col>
+              </Row>
+              <br />
+              <Row>
+                <Button onClick={this.submitschedule}>submit</Button>
+              </Row>
+
+              <Row>
+                <Col>
+                  {" "}
+                  Course Fees: Rs.{this.state.cardData.course_price}
+                  {this.state.showButton ? (
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      style={{ marginLeft: 15 }}
+                    >
+                      Pay
+                    </Button>
+                  ) : null}
+                </Col>
+              </Row>
+            </Panel>
+          ) : null}
         </Collapse>
         ,
       </div>
