cfdee52f1d8c60287e10aeba19709c30ca374c42
jai <jai.arora3@s.amity.edu>
2020-05-18 13:13:52 +0530

    Changes to footer and teacher.js and their css files

--- src/Component/homepage/teacher/teacher.js
+++ src/Component/homepage/teacher/teacher.js
@@ -7,34 +7,31 @@ class Teacher extends Component {
     return (
       <div
         style={{
-          backgroundColor: "#D3D3D3",
+          backgroundColor: "#fadde1",
           paddingTop: 30,
           paddingBottom: 50,
         }}
       >
         <Row
           justify="center"
-          style={{ fontSize: 40, fontWeight: "bolder", color: "#C64752" }}
+          style={{ fontSize: 50, fontWeight: "800", color: "#C64752", fontFamily: "'Roboto Slab', serif", textAlign: "center" }}
         >
-          How This Works !!!
+          How This Works
         </Row>
         <div className="main-div">
           <div className="child-div">
             <h3>REGISTER</h3>
-            <br />
             Want to teach something? Just register with some details and add
             atleast one course you specialise in and we'll find the students for
             you!
           </div>
           <div className="child-div">
             <h3>LOGIN AND UPDATE</h3>
-            <br />
             Keep adding courses you'd love to teach. Choose the time slots that
             you are comfortable with to teach
           </div>
           <div className="child-div">
             <h3>HAPPY TEACHING</h3>
-            <br />
             Based on the courses you've added, you can check your schedule
             depending upon the time slot chosen by the student and voila!
           </div>
--- src/Component/studentDashboard/progress.js
+++ src/Component/studentDashboard/progress.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
-import { Card,Row,Col } from "antd";
+import { Card } from "antd";
 import axios from "axios"
 import { Spin } from "antd";
 
 class progress extends Component {
-constructor(props){
-  super(props)
-  this.state={
-    enrolledCoursesArray:[],
-    loading:true
+  constructor(props) {
+    super(props)
+    this.state = {
+      enrolledCoursesArray: [],
+      loading: true
+    }
   }
-}
   componentDidMount() {
     const currentUser = JSON.parse(window.localStorage.getItem("currentUser"));
     const headers = { "x-auth-token": currentUser.token };
@@ -20,11 +20,11 @@ constructor(props){
         { headers }
       )
       .then((response) => {
-      console.log(response)
-      this.setState({loading:false})
-      for (const i in response.data) {
-        response.data[i].date = response.data[i].date.split("T")[0];
-      }
+        console.log(response)
+        this.setState({ loading: false })
+        for (const i in response.data) {
+          response.data[i].date = response.data[i].date.split("T")[0];
+        }
         this.setState({ enrolledCoursesArray: response.data });
       })
       .catch((error) => {
@@ -35,47 +35,33 @@ constructor(props){
     return (
       <Spin spinning={this.state.loading}>
 
-      <div>
-        <h5 style={{ marginBottom: 20 }}>Current Bought Courses: </h5>
-        {this.state.enrolledCoursesArray.map(course=>
-             <Card
-             style={{ marginTop: 16 }}
-             type="inner"
-             title={course.course_name}
-             key={course._id}
-             extra={<h5>By:{course.teacher_name} </h5>}
-           >
-             <Row justify="space-around">
-               <Col>              <p style={{ marginBottom: 0 }}><b>Teacher Mobile:</b> {course.teacher_mobile}</p>
-               </Col>
+        <div>
+          <h5 style={{ marginBottom: 20 }}>Current Bought Courses: </h5>
+          {this.state.enrolledCoursesArray.map(course =>
+            <Card
+              style={{ marginTop: 16 }}
+              type="inner"
+              title={course.course_name}
+              key={course._id}
+              extra={<h5>By:{course.teacher_name} </h5>}
+            >
+              <p style={{ marginBottom: 0 }}>Teacher Mobile: {course.teacher_mobile}</p>
+              <p style={{ marginBottom: 0 }}>Course Type: {course.course_type}</p>
+              <p style={{ marginBottom: 0 }}>Course Bought date: {course.date}</p>
 
-<Col>            <p style={{ marginBottom: 0 }}><b>Course Type:</b> {course.course_type}</p>
-</Col> 
-             </Row><br />
-             <Row justify="space-around">
-               <Col>
-             <p style={{ marginBottom: 0 }}><b>Course Bought date:</b> {course.date}</p>
-             </Col>
-             <Col>
-             <p> <b>Course Status:</b> {course.course_status ?<b style={{color:"green"}}>Completed</b> : <b style={{color:"blue"}}>Going on</b>  }</p>  
+              <p>Course Duration: {course.course_duration}</p>
+              {course.selected_course_schedule.map((j, index) => (
+                <p key={index}>
+                  {j.day} -- {j.time + ","}
+                </p>
+              ))}
 
-             </Col>
-             </Row>
-<br />
-                  <Row justify="center"><h5>Course Schedule</h5></Row>
-                  {course.selected_course_schedule.map((j,index) => (
-                <Row key={index} justify="center">
-                      {j.day} -- {j.time + ","}
-                      </Row>
-                  ))} 
-
-           
-           </Card>
+            </Card>
           )}
-     
-       
-        
-      </div>
+
+
+
+        </div>
       </Spin>
     );
   }
